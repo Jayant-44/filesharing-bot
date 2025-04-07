1ffd0504cd750c2baaef5b18d501fb54ca69271b
Jayant-44 <145659685+Jayant-44@users.noreply.github.com>
2025-04-07 10:38:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 # 
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "link2paisa.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "f40489eefb784d76fa03522ec7a9e11d2f9f42ae")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "e370a4bd310e114767337e3847cf71ade3470da5")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/+PZfHvzjSiZc4OGE1") # shareus ka tut_vid he 
