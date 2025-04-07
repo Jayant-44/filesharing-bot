dccf8309a1bf1580a53f3c88ebb7bbab92203d47
Jayant-44 <145659685+Jayant-44@users.noreply.github.com>
2025-04-07 11:19:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,9 +59,9 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official")
+        self.LOGGER(__name__).info(f"Bot Running..!")
         self.LOGGER(__name__).info(f""" \n\n       
-(っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
+(っ◔◡◔)っ ♥     Dramanavi   ♥
 ░╚════╝░░╚════╝░╚═════╝░╚══════╝
                                           """)
         self.username = usr_bot_me.username
